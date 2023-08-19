3f261ed1cb3ed1a0caf20fb2dcfbe62beeb8b83c
briyidta02 <briyidt.bautistaa@konradlorenz.edu.co>
2023-08-19 11:14:02 -0500

    add comment

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comment
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("the index is " + i);
+            }    
+            
+        }
+    }
+}
