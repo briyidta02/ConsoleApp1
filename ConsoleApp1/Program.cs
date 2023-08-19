71143e278f971df8c46f38bc0f6c6de84644bf17
briyidta02 <briyidt.bautistaa@konradlorenz.edu.co>
2023-08-19 12:35:18 -0500

    add comment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,28 @@
             string input = Console.ReadLine();
             int a = Int32.Parse(input);
             Console.WriteLine( a + 1);
-  
+
+           //ejercicio 2
+
+          Console.WriteLine("numero aleatorio");
+         
+            Random rnd = new Random();
+            int num = rnd.Next(0,200);
+            double resultado = (num * 30) / 100;
+
+            double resultadosuma = resultado + num;
+           
+            Console.Write(num);
+            Console.WriteLine("mas el aumento del 30 % es " + resultadosuma );
+         
+            // ejercicio 3
+
+            Console.WriteLine("ingrese base y altura del rectangulo");
+
+            
+           
+
             }
-        
+
         }
     }
